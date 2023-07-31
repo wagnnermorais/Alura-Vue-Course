c1f33641a394a02831bf65f6f1081f2ba9926ba4
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-07-30 21:53:55 -0300

    feat: new image form

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -1,21 +1,94 @@
+<!-- alurapic/src/components/cadastro/Cadastro.vue -->
+
 <template>
   <div>
-    <h2>Content</h2>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado"></h2>
+
+    <form @submit.prevent="grava()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off" v-model="foto.titulo" />
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" v-model.lazy="foto.url" />
+        <image-component :url="foto.url" />
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea
+          id="descricao"
+          autocomplete="off"
+          v-model="foto.descricao"
+        ></textarea>
+      </div>
+
+      <div class="centralizado">
+        <Button rotulo="GRAVAR" tipo="submit" />
+        <router-link to="/"
+          ><Button rotulo="VOLTAR" tipo="button"
+        /></router-link>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Cadastro",
+import Imagens from "@/components/Imagens.vue";
+import Button from "@/components/Button.vue";
+import Foto from "@/domain/Foto";
+import Service from "@/domain/Service";
 
+export default {
   data() {
-    return {};
+    return {
+      foto: new Foto(),
+    };
+  },
+
+  methods: {
+    grava() {
+      this.service.cadastra(this.foto).then(
+        () => (this.foto = new Foto()),
+        (err) => console.log(err)
+      );
+    },
+  },
+
+  created() {
+    this.service = new Service(this.$resource);
+  },
+
+  components: {
+    "image-component": Imagens,
+    Button,
   },
 };
 </script>
-
 <style scoped>
-div {
-  background: dodgerblue;
+.centralizado {
+  text-align: center;
+}
+.controle {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+.controle label {
+  display: block;
+  font-weight: bold;
+}
+
+.controle label + input,
+.controle textarea {
+  width: 100%;
+  font-size: inherit;
+  border-radius: 5px;
+}
+
+.centralizado {
+  text-align: center;
 }
 </style>
