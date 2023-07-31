61098a128cf1f32c1d05b34012eb7eab7165b25d
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-07-30 21:56:23 -0300

    feat: requests using services

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import VueResource from "vue-resource";
 import "./directives/Transform";
 
 Vue.use(VueResource);
+Vue.http.options.root = "http://localhost:3000";
 Vue.config.productionTip = false;
 
 new Vue({
